9b8b79dea26875b0d4e09d874c7afe1d867acf73
Ric-Lavers <ric_lavers@outlook.com>
2018-11-10 16:29:26 +1100

    ...start

--- src/data/controllers/demo.js
+++ src/data/controllers/demo.js
@@ -1,16 +1,14 @@
 import { createDemo } from '../models/demoModel'
 
 const getDemo = (req, res) => {
-  console.log("getDemo")
   try {
-    res.send({demo: "success!"})
+    res.send({get_request_demo: "success!"})
   } catch (error) {
     res.send(error)
   }
 };
 
 const postDemo = async (req, res) => {
-  console.log("postDemo", req.body)
   try {
     const response = await createDemo(req.body)
     res.send(response)
