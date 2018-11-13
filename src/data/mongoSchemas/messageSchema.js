6d97dd51df1ad218327464adde825db10d28cae3
Ric-Lavers <ric_lavers@outlook.com>
2018-11-13 22:22:48 +1100

    extend personSchema

--- src/data/mongoSchemas/messageSchema.js
+++ src/data/mongoSchemas/messageSchema.js
@@ -5,7 +5,7 @@ const Schema = mongoose.Schema
 const messageSchema = new Schema({
 	name: String,
 	message: String,
-	personId: String,
+  personId: String,
   created_on: {
     type: Date,
     default: Date.now()
--- src/data/mongoSchemas/personSchema.js
+++ src/data/mongoSchemas/personSchema.js
@@ -5,7 +5,9 @@ const Schema = mongoose.Schema
 const personSchema = new Schema({
 	name: String,
 	email: String,
-	phoneNumber: String,
+  phoneNumber: String,
+  bed: Boolean,
+  car: Boolean,
   created_on: {
     type: Date,
     default: Date.now()
